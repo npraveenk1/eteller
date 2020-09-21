c130dc8f9b50a11fe1b1044c244383ea6caf6b46
angular-cli <angular-cli@angular.io>
2020-09-20 22:53:28 -0400

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { EtellerPage } from './app.po';
+
+describe('eteller App', function() {
+  let page: EtellerPage;
+
+  beforeEach(() => {
+    page = new EtellerPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
